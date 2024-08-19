c2819d10af0b756702e4526bc6d070855b8e8398
Rafael <rr65887@gmail.com>
2024-08-19 11:59:35 -0300

    last update to finish the project

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () => {
+    try {
+        
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@reactgram.mzeqk3j.mongodb.net/?retryWrites=true&w=majority&appName=ReactGram`,
+        );
+
+        console.log("Conectou ao banco!")
+
+        return dbConn;
+    } catch (error){
+        console.log(error)
+    }
+};
+
+conn();
+
+module.exports = conn;
